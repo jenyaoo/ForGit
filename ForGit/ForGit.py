a8adf543e3f66ca252f251127c12b7892d33aec9
Евгений Костык <jenya.simf2017@gmail.com>
2021-11-20 09:43:09 +0300

    new

--- ForGit/ForGit.py
+++ ForGit/ForGit.py
@@ -1,7 +1,16 @@
 import turtle
-a=[0]*10
-turtle.speed(5000)
-for i in range(10):
-    a[i]=int(input())
-for i in range(10):
-    turtle.circle(a[0])
+a=list(map(int,input().split()))
+m=0
+ind=0
+turtle.speed(500)
+for i in range(len(a)):
+    if m<a[i]:
+        ind=i
+        m=a[i]
+for i in range(len(a)):
+    if i==ind:
+        turtle.color(1,0,0)
+        turtle.circle(a[i])
+        turtle.color(1,1,1)
+    else:
+        turtle.circle(a[i])
