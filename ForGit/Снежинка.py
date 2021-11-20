adc02aca492cc190b185cb1128dc62904ad3ecce
Евгений Костык <jenya.simf2017@gmail.com>
2021-11-20 10:30:55 +0300

    Add color

--- ForGit/Снежинка.py
+++ ForGit/Снежинка.py
@@ -1,7 +1,12 @@
 import turtle
 turtle.speed(500)
+from random import randint
+
+def Chc():
+    turtle.color(randint(1,999)/1000,randint(1,999)/1000,randint(1,999)/1000)
 
 def w(a,i):
+    Chc()
     if i>0:
         w(a//2,i-1)
     else:
