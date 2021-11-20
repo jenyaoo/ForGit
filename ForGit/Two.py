9afba09777b5569df6e53ac2b2d7e9162e8e7e29
Евгений Костык <jenya.simf2017@gmail.com>
2021-11-20 10:15:18 +0300

    optimise

--- ForGit/Two.py
+++ ForGit/Two.py
@@ -3,14 +3,9 @@ turtle.speed(500)
 
 def sq(a,b):
     if b!=0:    
-        turtle.forward(a)
-        turtle.right(90)
-        turtle.forward(a)
-        turtle.right(90)
-        turtle.forward(a)
-        turtle.right(90)
-        turtle.forward(a)
-        turtle.right(90)
+        for i in range(4):
+            turtle.forward(a)
+            turtle.right(90)
     else: 
         return 0
     return(sq(a//2,b-1))
